cb13c5cfb9a5af4f6db587c3607ca33ab3753da1
NuelGit <zioneemma@gmail.com>
2023-04-06 12:43:05 +0100

    Changed Codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AddUser from './components/Users/AddUser';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <AddUser />
     </div>
   );
 }
